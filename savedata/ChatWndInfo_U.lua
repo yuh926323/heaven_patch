479ee1956c54aa56fead3aa4460d6cd4a1663832
Young <yuh926@ecommaxtech.com>
2017-11-21 23:47:01 +0800

    patch init

---
+++ savedata/ChatWndInfo_U.lua
@@ -0,0 +1,32 @@
+CanDragWnd = 1
+
+ChatSubWnd_1 = {
+	XPos = 0,
+	YPos = 0,
+	Width = 0,
+	Height = 0,
+	TabState = "On",
+	TabName = [[¾Ô§Ð°T®§]],
+	option1 = 1,
+	option2 = 1,
+	option3 = 1,
+	option4 = 1,
+	option5 = 1,
+	option6 = 1,
+	option7 = 1,
+	option8 = 1,
+	option9 = 1,
+	option10 = 1,
+	option11 = 1,
+	option12 = 1,
+	option13 = 1,
+	option14 = 1,
+	option15 = 1,
+	option16 = 0,
+	option17 = 0,
+	option18 = 0,
+	option19 = 1,
+	option20 = 1,
+	option21 = 1,
+}
+
